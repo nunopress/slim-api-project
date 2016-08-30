9cd25e34adf6237d4af9cb8f6c60c2b6f3e2c223
Pereira Ricardo <hello@nunopress.com>
2016-08-30 18:17:54 +0200

    Revert back without checking of MiddlewareController parent.

--- private/src/NunoPress/Slim/App.php
+++ private/src/NunoPress/Slim/App.php
@@ -9,13 +9,13 @@ namespace NunoPress\Slim;
 class App extends \Slim\App
 {
     /**
-     * @param $pattern
-     * @param $name
-     * @param MiddlewareController $controller
+     * @param string $pattern
+     * @param string $name
+     * @param string $controller
      * @param array $methods
      * @return $this
      */
-    public function resource($pattern, $name, MiddlewareController $controller, array $methods = [])
+    public function resource($pattern, $name, $controller, array $methods = [])
     {
         /* @todo: rewrite the key association
         $mappedMethods = [
